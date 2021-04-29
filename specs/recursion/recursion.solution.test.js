9678172c607be0a67af60992423e7988e66da0db
Brian Holt <btholt@gmail.com>
2021-04-28 20:17:29 -0700

    Initial commit

---
+++ specs/recursion/recursion.solution.test.js
@@ -0,0 +1,25 @@
+/*
+  Make a function that computes a factorial recursively.
+  A factorial is when you take a number n and multiply by each preceding integer until you hit one.
+  n * (n-1) * (n-2) ... * 3 * 2 * 1
+  
+  Call the function factorial
+  
+  factorial(1) = 1
+  factorial(2) = 2
+  factorial(3) = 6 
+*/
+
+function factorial(num) {
+  if (num < 2) return 1;
+  return num * factorial(num - 1);
+}
+
+// unit tests
+// do not modify the below code
+test("factorials", () => {
+  expect(factorial(1)).toEqual(1);
+  expect(factorial(2)).toEqual(2);
+  expect(factorial(3)).toEqual(6);
+  expect(factorial(10)).toEqual(3628800);
+});
---
+++ specs/recursion/recursion.test.js
@@ -0,0 +1,22 @@
+/*
+  Make a function that computes a factorial recursively.
+  A factorial is when you take a number n and multiply by each preceding integer until you hit one.
+  n * (n-1) * (n-2) ... * 3 * 2 * 1
+  
+  Call the function factorial
+  
+  factorial(1) = 1
+  factorial(2) = 2
+  factorial(3) = 6 
+*/
+
+function factorial(n) {}
+
+// unit tests
+// do not modify the below code
+test.skip("factorials", () => {
+  expect(factorial(1)).toEqual(1);
+  expect(factorial(2)).toEqual(2);
+  expect(factorial(3)).toEqual(6);
+  expect(factorial(10)).toEqual(3628800);
+});
