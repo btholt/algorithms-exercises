81dacea452622d2f045d46c9152ef70a89d52c9c
Brian Holt <btholt@gmail.com>
2021-05-03 21:30:38 -0700

    insertion

--- specs/insertion-sort/insertion-sort.solution.test.js
+++ specs/insertion-sort/insertion-sort.solution.test.js
@@ -7,22 +7,22 @@
   The outer loop goes over the whole list, the index of which signifies where the "sorted" part of the list is. The inner
   loop goes over the sorted part of the list and inserts it into the correct position in the array.
   
-  Like bubble sort, there's a visualization mechanism available to you. Just call snapshot(myArray) at the beginning of
-  your inner loop and it should handle the rest for you!
-  
-  And you put xdescribe instead of describe if you want to suspend running the unit tests.  
 */
-
 function insertionSort(nums) {
   for (let i = 1; i < nums.length; i++) {
-    for (let j = 0; j < i; j++) {
-      if (nums[i] < nums[j]) {
-        const [spliced] = nums.splice(i, 1);
-        nums.splice(j, 0, spliced);
-        j = i; // break the inner loop
-      }
+    let numberToInsert = nums[i]; // the numberToInsert number we're looking to insert
+    let j; // the inner counter
+
+    // loop from the right to the left
+    for (j = i - 1; nums[j] > numberToInsert && j >= 0; j--) {
+      // move numbers to the right until we find where to insert
+      nums[j + 1] = nums[j];
     }
+
+    // do the insertion
+    nums[j + 1] = numberToInsert;
   }
+  return nums;
 }
 
 // unit tests
