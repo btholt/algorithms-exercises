e1bdfe31ac33b78ac2e9503741be1b2eb6fab560
Brian Holt <btholt@gmail.com>
2021-05-04 21:57:03 -0700

    merge sort

--- specs/merge-sort/merge-sort.solution.test.js
+++ specs/merge-sort/merge-sort.solution.test.js
@@ -3,39 +3,41 @@
   Name the function mergeSort
   It will take in a array of numbers and return a sorted array numbers
   
-  To read the approach, refer to the class materials at 
-  https://btholt.github.io/four-semesters-of-cs/
-  
-  As always, you can rename describe to be xdescribe to prevent the
-  unit tests from running while you're working
-  
-  There is no visualization mechanism for this algorithm. Use your own
-  preferred method of introspection like console.log().
+  You'll need to write more than just one function
 */
 
 const mergeSort = (nums) => {
+  // base case
   if (nums.length < 2) {
     return nums;
   }
+
+  // you can be more clever about this code but I wanted it
+  // to be readable to you
   const length = nums.length;
   const middle = Math.floor(length / 2);
   const left = nums.slice(0, middle);
   const right = nums.slice(middle);
 
+  // merge takes two sorted lists and returns one sorted list
   return merge(mergeSort(left), mergeSort(right));
 };
 
 const merge = (left, right) => {
   const results = [];
 
+  // go until one list runs outs
   while (left.length && right.length) {
     if (left[0] <= right[0]) {
+      // shift removes the first element in an array and returns it
+      // it's like .pop() for the front
       results.push(left.shift());
     } else {
       results.push(right.shift());
     }
   }
 
+  // either left or right will be empty so you can safely concat both
   return results.concat(left, right);
 };
 
--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -2,15 +2,8 @@
   Write a function that performs mergesort
   Name the function mergeSort
   It will take in a array of numbers and return a sorted array numbers
-  
-  To read the approach, refer to the class materials at 
-  https://btholt.github.io/four-semesters-of-cs/
-  
-  As always, you can rename describe to be xdescribe to prevent the
-  unit tests from running while you're working
-  
-  There is no visualization mechanism for this algorithm. Use your own
-  preferred method of introspection like console.log().
+
+  You'll need to write more than just one function
 */
 
 const mergeSort = (nums) => {
