4973e38e2f23343fbdcbef01b1fd981c656dc1bf
Brian Holt <btholt@gmail.com>
2021-06-02 11:26:41 -0500

    comment out log maze

--- specs/pathfinding/pathfinding.solution.test.js
+++ specs/pathfinding/pathfinding.solution.test.js
@@ -27,7 +27,7 @@ const findShortestPathLength = (maze, [xA, yA], [xB, yB]) => {
   );
   visited[yA][xA].openedBy = BY_A;
   visited[yB][xB].openedBy = BY_B;
-  logMaze(visited);
+  // logMaze(visited);
 
   let aQueue = [visited[yA][xA]];
   let bQueue = [visited[yB][xB]];
@@ -69,7 +69,7 @@ const findShortestPathLength = (maze, [xA, yA], [xB, yB]) => {
         bQueue.push(neighbor);
       }
     }
-    logMaze(visited);
+    // logMaze(visited);
   }
   return -1;
 };
