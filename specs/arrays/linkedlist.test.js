024842adc615564f3de380a66e1cc6e2e21c8914
Brian Holt <btholt@gmail.com>
2021-06-03 13:41:39 -0500

    new linkedlist

--- specs/arrays/linkedlist.test.js
+++ specs/arrays/linkedlist.test.js
@@ -24,16 +24,70 @@
 */
 
 class LinkedList {
-  // code goes here
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(value) {
+    const node = new Node(value);
+    this.length++;
+    if (!this.head) {
+      this.head = node;
+    } else {
+      this.tail.next = node;
+    }
+    this.tail = node;
+  }
+  pop() {
+    return this.delete(this.length - 1);
+  }
+  _find(index) {
+    if (index >= this.length) return null;
+    let current = this.head;
+    for (let i = 0; i <= index - 1; i++) {
+      current = current.next;
+    }
+
+    return current;
+  }
+  get(index) {
+    const node = this._find(index);
+    if (!node) return void 0;
+    return node.value;
+  }
+  delete(index) {
+    if (index === 0) {
+      const head = this.head;
+      if (head) {
+        this.head = head.next;
+      } else {
+        this.head = null;
+      }
+      this.length--;
+      return head.value;
+    }
+
+    const node = this._find(index - 1);
+    const excise = node.next;
+    if (!excise) return null;
+    node.next = excise.next;
+    if (!node.next) this.tail = node.next;
+    this.length--;
+    return excise.value;
+  }
 }
 
 class Node {
-  // code goes here
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
 // unit tests
 // do not modify the below code
-describe.skip("LinkedList", function () {
+describe("LinkedList", function () {
   const range = (length) =>
     Array.apply(null, { length: length }).map(Number.call, Number);
   const abcRange = (length) =>
