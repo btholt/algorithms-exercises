70dad5c289bf760f652c4ffd26911b19dad4f587
Owen Wheatley <owen.pf.wheatley@gmail.com>
2021-08-19 22:24:01 +0100

    fix bubble sort jest text so it passes

--- specs/bubble-sort/bubble-sort.solution.test.js
+++ specs/bubble-sort/bubble-sort.solution.test.js
@@ -32,5 +32,5 @@ function bubbleSort(nums) {
 test("bubble sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   bubbleSort(nums);
-  expect(nums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  expect(nums).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 });
