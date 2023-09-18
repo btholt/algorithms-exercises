990716a20e44604f49a499fcfa6b80b1ed588318
Sofiyullah Abdullah <66748323+Precioussoul@users.noreply.github.com>
2023-09-18 22:23:05 +0100

    initial commit

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -12,11 +12,26 @@
 
 function bubbleSort(nums) {
   // code goes here
+  let swapped = false;
+
+  do {
+    swapped = false;
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] > nums[i + 1]) {
+        const temps = nums[i + 1];
+        nums[i + 1] = nums[i];
+        nums[i] = temps;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+
+  return nums;
 }
 
 // unit tests
 // do not modify the below code
-test.skip("bubble sort", function () {
+test("bubble sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const sortedNums = bubbleSort(nums);
   expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
